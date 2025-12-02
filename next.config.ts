b219338f0f59f96f9c78602ade140d7f6caf3f10
Lukaszglaz <glazzlukasz@gmail.com>
2025-12-02 10:21:49 +0100

    refactor-test(next.config): remove redirects for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    const isDevOrPreview = process.env.VERCEL_ENV !== "production";
-
-    if (isDevOrPreview) {
-      return [
-        {
-          source: "/",
-          destination: "/v2",
-          permanent: false,
-        },
-      ];
-    }
-    return [];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
