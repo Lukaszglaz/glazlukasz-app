d1e3f44ce418d6c552e7c16891ea009eb80a620f
Lukaszglaz <glazzlukasz@gmail.com>
2025-12-02 10:21:00 +0100

    feat-test(config): add redirects for development and preview environments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    const isDevOrPreview = process.env.VERCEL_ENV !== "production";
+
+    if (isDevOrPreview) {
+      return [
+        {
+          source: "/",
+          destination: "/v2",
+          permanent: false,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
