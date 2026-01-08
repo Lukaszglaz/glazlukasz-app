d5b93fa1d754a50be37cecd6617e98d2266602dc
Lukaszglaz <glazzlukasz@gmail.com>
2026-01-08 22:18:01 +0100

    feat(offerDataLarge): add new offer data structure and integrate into Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,35 +10,9 @@ import projectOneImage from "@/assets/images/showCaseCard/ProjectOne.jpg";
 import projectTwoImage from "@/assets/images/showCaseCard/ProjectTwo.jpg";
 import { ContactFormCard } from "@/components/shared/ContactFormCard/ContactFormCard";
 import { offerData } from "./data/homePage/offerData";
+import { offerDataLarge } from "./data/homePage/offerDataLarge";
 
 export default function Home() {
-  const offerDataLarge = [
-    {
-      title: "Strony Internetowe Skoncentrowane na Wyniku",
-      description:
-        "Tworzę witryny, które nie tylko **wyglądają świetnie**, ale są strategicznie zaprojektowane, aby **działać doskonale** – zoptymalizowane pod kątem szybkiej konwersji i intuicyjnej obsługi.",
-      icon: Layout,
-    },
-    {
-      title: "Profesjonalne Projektowanie Wizualne (UI/UX)",
-      description:
-        "Zajmuję się projektowaniem każdego detalu, dbając, by Twoja strona była **estetyczna, funkcjonalna i przyjazna** dla użytkowników. Zadbaj o to, by przyciągała uwagę od pierwszego wejrzenia.",
-      icon: Palette,
-    },
-    {
-      title: "Spójny Branding i Identyfikacja Wizualna",
-      description:
-        "Buduj **zaufanie i lojalność** klientów. Oferuję wsparcie w kreowaniu spójnego i przemyślanego **brandingu**, który odzwierciedla wartości i osobowość Twojego biznesu.",
-      icon: Zap,
-    },
-    {
-      title: "Strategiczne Konsultacje Projektowe",
-      description:
-        "Potrzebujesz pewności co do designu lub **strategii wizualnej**? Chętnie podzielę się wiedzą i doświadczeniem, aby wspólnie znaleźć **najlepsze i najbardziej efektywne rozwiązania** dla Twojego projektu.",
-      icon: Handshake,
-    },
-  ];
-
   const benefitData = [
     {
       stepNumber: 1,
